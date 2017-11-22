8f87ac145f2dbe5ae9b09f3ac7031c00f5f9e596
ADI\lugonch <christophe.lugon@fhvi.ch>
2017-11-22 17:11:40 +0100

    Add Admin LTE Template

--- Budget/App_Start/BundleConfig.cs
+++ Budget/App_Start/BundleConfig.cs
@@ -8,46 +8,56 @@ namespace Budget
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-2.1.1.js",
+            bundles.Add(new ScriptBundle("~/Scripts/scripts").Include(
+                        "~/Scripts/jquery.js",
+                        "~/Scripts/jquery-ui.min.js",
                         "~/Scripts/jquery.dataTables.js",
-                        "~/Scripts/jquery-ui/jquery-ui.js",
+                        // "~/Scripts/jquery-sortable.js",
                         "~/Scripts/angular.min.js",
                         "~/Scripts/angular-mocks.js",
-                        "~/Scripts/angular-ui-bootstrap.js"
+                        "~/Scripts/angular-ui-bootstrap.js",
+                        "~/Scripts/bootstrap.min.js",
+                        "~/Scripts/raphael.min.js",
+                        "~/Scripts/morris.min.js",
+                        "~/Scripts/jquery.sparkline.min.js",
+                        "~/Scripts/jquery-jvectormap-1.2.2.min.js",
+                        "~/Scripts/jquery-jvectormap-world-mill-en.js",
+                        "~/Scripts/jquery.knob.min.js",
+                        "~/Scripts/moment-with-locales.min.js",
+                        "~/Scripts/daterangepicker.js",
+                        "~/Scripts/bootstrap-datepicker.min.js",
+                        "~/Scripts/bootstrap3-wysihtml5.all.min.js",
+                        "~/Scripts/jquery.slimscroll.min.js",
+                        "~/Scripts/fastclick.js",
+                        "~/Scripts/adminlte.min.js",
+                        "~/Scripts/dashboard.js",
+                        "~/Scripts/dataTables/dataTables.bootstrap4.js",
+                        "~/Scripts/toastr.min.js",
+                        "~/Scripts/jsgrid/jsgrid.js",
+                        "~/Scripts/jsgrid/i18n/jsgrid-fr.js",
+                        "~/Scripts/simple-iconpicker.js",
+                        "~/Scripts/xeditable/xeditable.js",
+                        "~/Scripts/site.js",
+                        "~/Scripts/app/transaction-controller.js"
                         ));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap/bootstrap.js",
-                      "~/Scripts/respond.js"
-                      ));
-
-            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
-                      "~/Scripts/dataTables/dataTables.bootstrap4.js",
-                      // "~/Scripts/chart.js/Chart.min.js",
-                      "~/Scripts/sb/sb-admin-datatables.min.js",
-                      "~/Scripts/sb/sb-admin.min.js",
-                      "~/Scripts/toastr.min.js",
-                      "~/Scripts/jsgrid/jsgrid.js",
-                      "~/Scripts/jsgrid/i18n/jsgrid-fr.js",
-                      "~/Scripts/simple-iconpicker.js",
-                      "~/Scripts/jquery-sortable.js",
-                      "~/Scripts/xeditable/xeditable.js",
-                      "~/Scripts/app/transaction-controller.js"
-                      // "~/Scripts/sb/sb-admin-charts.min.js"
-                      ));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap/bootstrap.css",
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/font-awesome.min.css",
+                      "~/Content/ionicons.min.css",
+                      "~/Content/AdminLTE.min.css",
+                      "~/Content/_all-skins.min.css",
+                      "~/Content/morris.css",
+                      "~/Content/jquery-jvectormap.css",
+                      "~/Content/bootstrap-datepicker.min.css",
+                      "~/Content/bootstrap3-wysihtml5.min.css",
                       "~/Content/jquery-ui/jquery-ui.css",
                       "~/Content/jquery-ui/jquery-ui.structure.css",
                       "~/Content/jquery-ui/jquery-ui.theme.css",
                       "~/Content/dataTables/dataTables.bootstrap4.css",
-                      "~/Content/font-awesome/font-awesome.min.css",
                       "~/Content/toastr.min.css",
                       "~/Content/jsgrid/jsgrid.min.css",
                       "~/Content/jsgrid/jsgrid-theme.css",
-                      "~/Content/sb/sb-admin.css",
                       "~/Content/simple-iconpicker.css",
                       "~/Content/xeditable/xeditable.css",
                       "~/Content/site.css"
